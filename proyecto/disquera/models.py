108b354f788e282237da7a3535abd78e08a90f10
Erick Hernández <84101451+ErickFHP@users.noreply.github.com>
2024-05-15 16:25:05 -0600

    Renovacion

--- proyecto/disquera/models.py
+++
@@ -1,8 +0,0 @@
-from django.db import models
-
-# Create your models here.
-class Prueba(models.Model):
-    id = models.AutoField(primary_key=True)
-    nombre = models.CharField(max_length=255, null=False, verbose_name="Nombre del Artista")
-    region = models.CharField(max_length=255, null=False, verbose_name="Región del Artista")
-
